014480475a26e040a9900898d59e4b57a442d88d
shanky17 <shashaank.soni@gmail.com>
2024-11-26 19:08:31 +0530

    Added httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './main-app/header/header.component';
 import { FooterComponent } from './main-app/footer/footer.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  private http = inject(HttpClient);
 
+  ngOnInit() {
+    this.fetchHttpData();
+  }
+
+  async fetchHttpData() {
+    // this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe({
+    //   next: (posts) => {
+    //     console.log(posts);
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //   },
+    // });
+
+    try {
+      const posts = await lastValueFrom(
+        this.http.get('https://jsonplaceholder.typicode.com/posts')
+      );
+      console.log(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
